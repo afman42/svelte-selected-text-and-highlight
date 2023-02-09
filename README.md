d01ad9ead0e4f27d2a4dbb7f1f3780408e102bd0
afman42 <afifarman50@gmail.com>
2023-02-09 11:30:37 +0700

    add feature save in localstorage

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ just like reader ebook app functional
 - can
   - selected text with button 
   - click text color and show
+  - after hightlight can remove double click
+  - store in the local storage
 
 - can't
-  - store in the local storage
   - add anotation in highlight text
--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,37 @@ let selection = ``;
 let openSelectText = false;
 let setObjectText = "";
 let warnSelection = false;
+let collectTextHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
+let ref;
 
 onMount(() => {
   document.addEventListener('selectionchange',() => {
     selection = document.getSelection();
   })
+  document.addEventListener('DOMContentLoaded',() => {
+    let spanAll = document.querySelectorAll('span')
+    if (spanAll.length > 0) {
+      spanAll.forEach(v => {
+        v.addEventListener('click', (e) => {
+          if(e.target.outerHTML.includes(e.target.dataset.textz)){
+            setObjectText = e.target.dataset.textz;
+          }
+        })
+        v.addEventListener('dblclick', (e) => {
+          if(e.target.outerHTML.includes(e.target.dataset.textz)){
+            let fromLocalStorage = localStorage.getItem('collectTextHTML')
+            let repText = fromLocalStorage.replace(e.target.outerHTML,e.target.innerText);
+            localStorage.setItem('collectTextHTML',repText)
+            collectTextHTML = repText
+            setObjectText = "";
+          }
+        })
+      })
+    }
+  })
+  if (localStorage.getItem('collectTextHTML') != null) {
+    collectTextHTML = localStorage.getItem('collectTextHTML');
+  }
 })
 
 function selectionSlice(color){
@@ -19,12 +45,24 @@ function selectionSlice(color){
     warnSelection = false;
     for (var i = 0; i < selection.rangeCount; i++) {
         text = `${color} ${selection}`;
-        var $sNode = selection.getRangeAt(i).startContainer.parentNode;
-        var $eNode = selection.getRangeAt(i).endContainer.parentNode;
+        let $sNode = selection.getRangeAt(i).startContainer.parentNode;
+        //var $eNode = selection.getRangeAt(i).endContainer.parentNode;
         $sNode.innerHTML = $sNode.innerHTML.replace(selection,`<span style='cursor:pointer;background-color:${color};' data-textZ='${text}'>${selection}</span>`);
+        localStorage.setItem('collectTextHTML',$sNode.innerHTML);
         $sNode.onclick = function(e){
-          openSelectText = !openSelectText;
-          setObjectText = e.target.dataset.textz;
+          e.preventDefault();
+          if(e.target.outerHTML.includes(e.target.dataset.textz)){
+            setObjectText = e.target.dataset.textz;
+          }
+        }
+        $sNode.ondblclick = function(e){
+          e.preventDefault();
+          //console.log(e);
+
+          if(e.target.outerHTML.includes(e.target.dataset.textz)){
+            $sNode.innerHTML = $sNode.innerHTML.replace(e.target.outerHTML,e.target.innerText);
+            setObjectText = "";
+          }
         }
     }
   }
@@ -41,10 +79,8 @@ function selectionSlice(color){
     <button type="button" on:click={() => selectionSlice('blue')}>blue</button>
     <button type="button" on:click={() => selectionSlice('red')}>red</button>
   </div>
-  <div>
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-  </div>
-  {#if openSelectText}
+  <div bind:this={ref} class="collectDiv">{@html collectTextHTML}</div>
+  {#if setObjectText != ""}
     <div class="openSelectText">
       <div>#{setObjectText}</div>
     </div>
@@ -73,6 +109,7 @@ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
 
 .openSelectText {
   display:flex;
-  flex-direction: row;
+  flex-direction: column;
+  margin-top:5px;
 }
 </style>
